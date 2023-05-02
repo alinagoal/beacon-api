6dfeaea74bace4292839a55416ab17f64c85c341
alinagoal <alidangol71@gmail.com>
2023-05-02 22:10:47 +0545

    feat: beacon api try

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express = require("express");
+const bodyParser = require('body-parser')
 const app = express();
 
-app.get("/", (req, res) => res.sendFile(`${__dirname}/index.html`))
-app.get("/img/:id", (req, res)=> {
-    const id = req.params.id;
-    res.sendFile(`${__dirname}/img/${id}`)
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.json()) 
+app.use(bodyParser.text())
+
+// app.get("/", (req, res) => res.sendFile(`${__dirname}/index.html`))
+app.get("/", (req, res) => res.sendFile(`C:\\Users\\adang\\beacon-api\\fe.html`))
+// app.get("/img/:id", (req, res)=> {
+//     const id = req.params.id;
+//     res.sendFile(`${__dirname}/img/${id}`)
+// })
+
+// ----url-------/endpoint/{pathparmas}?q=q1&w=q2
+// localhost:port/endpoint?id1=asdsad&id2=asdassdsf
+
+app.get('/alina', (req, res ) => {
+	console.log('im alina')
+	res.send({'suuccess':true})
 })
 
-app.post("/", (req, res) => {
-    console.log(`id ${req.query.id} timespent ${req.query.timespent} ms`)
-    res.sendStatus(204);
+app.post("/beacon", (req, res) => {
+    console.log(`body`, req.body)
+    console.log(`query`, req.query)
+	res.send({ title: 'GeeksforGeeks' });
 })
-app.listen(8080, ()=>console.log("Listening on 8080"))
+app.listen(4000, ()=>console.log("Listening on 4000"))
+
+
